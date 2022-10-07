860b5908f413640070d163e32cb211dc5b312379
Jason Hartsell <j.hartsell@astoundcommerce.com>
2022-10-06 18:20:41 -0700

    create sign-up component, form input and button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
+        await createUserDocumentFromAuth(user);
     };
 
     return (
@@ -12,6 +14,7 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign In with Google Popup
             </button>
+            <SignUpForm />
         </div>
     )
 };
